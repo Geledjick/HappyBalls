bdd08d73b73a3cfaa07d61fd32b3b985fe63b991
Geledjick <154546497+Geledjick@users.noreply.github.com>
2026-02-07 22:42:20 +0500

    first stable version

---
+++ main.cpp
@@ -0,0 +1,261 @@
+// SFML
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+// Other
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <optional>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+struct Ball {
+    enum Type {
+        None,
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        TypeCount
+    } type;
+
+    sf::Color getColor() {
+        switch (type) {
+            case Red: return sf::Color::Red;
+            case Green: return sf::Color::Green;
+            case Blue: return sf::Color::Blue;
+            case Yellow: return sf::Color::Yellow;
+            default: return sf::Color::Transparent;
+        }
+    }
+};
+
+class Game {
+public:
+    Game() : 
+        window(sf::VideoMode({800, 900}), "HappyBalls", sf::Style::Close | sf::Style::Titlebar), 
+        gameOverText(font),
+        scoreText(font)
+    {
+        window.setFramerateLimit(60);
+        sf::Vector2u resolution = window.getSize();
+        window.setPosition({(int)resolution.x, (int)resolution.y / 2});        
+        
+        srand(time(NULL));
+
+        font.setSmooth(true);
+        if (!font.openFromFile("fonts/Hack-Regular.ttf")) {
+            exit(1);
+        }
+
+        gameOverText.setString("GAME OVER");
+        gameOverText.setCharacterSize(48);
+        gameOverText.setFillColor(sf::Color::Red);
+        gameOverText.setPosition({
+            float(resolution.x - gameOverText.getCharacterSize() * gameOverText.getString().getSize()), 
+            float(resolution.y - gameOverText.getCharacterSize() * 1.75),
+        });
+
+        scoreText.setCharacterSize(48);
+        scoreText.setFillColor(sf::Color(60, 62, 80));
+        scoreText.setPosition({
+            float(scoreText.getCharacterSize()),
+            float(resolution.y - scoreText.getCharacterSize() * 1.75),
+        });
+
+        field.resize(cols);
+        for (std::vector<Ball> &row : field) {
+            row.resize(rows);
+            for (Ball &ball : row) {
+                ball.type = Ball::Type::None;
+            }
+        }
+
+        int rs = resolution.x / rows;
+        int cs = resolution.y / cols;
+        tileSize = (rs > cs ? cs : rs);
+        ballSize = tileSize * 0.33f;
+        ballCenterPosition = ballSize / 2.f;
+
+        ball.setRadius(ballSize);
+
+        tile.setFillColor(sf::Color(50, 52, 70));
+        tile.setSize({tileSize, tileSize});
+        tile.setOutlineColor(sf::Color(68, 71, 90));
+        tile.setOutlineThickness(outlineSize);
+    }
+
+    void run() {
+        generateBalls();
+        while (window.isOpen()) {
+            handleEvents();
+            render();
+            checkField();
+        }        
+    }
+
+private:
+    sf::RenderWindow window;
+
+    std::vector<std::vector<Ball>> field;
+
+    const int rows = 12;
+    const int cols = 12;
+
+    float tileSize;
+    float ballSize;
+    float ballCenterPosition;
+    sf::RectangleShape tile;
+    sf::CircleShape ball;
+    const int outlineSize = 2.f;
+    const int spaceOutlineSize = outlineSize * 2;
+
+    const sf::Color backgroundColor = sf::Color(40, 42, 54);
+
+    const int countGenerateBalls = (rows + cols) / 4;
+    const int maxGeneratorIterations = rows * cols;
+
+    int score = 0;
+    bool gameOver = false;
+    std::optional<sf::Event> event;
+
+    sf::Font font;
+    sf::Text scoreText;
+    sf::Text gameOverText;
+
+    sf::Vector2i lastMousePosition;
+    bool lastMouseClickState = false;
+    Ball grabedBall{Ball::Type::None};
+
+    void handleEvents() {
+        while ((event = window.pollEvent())) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+                return;
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && !lastMouseClickState && !gameOver) {
+                lastMouseClickState = true;
+            } else if (!sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && lastMouseClickState && !gameOver) {
+                lastMouseClickState = false;
+                mouseClick();
+                //generateBalls();
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R)) {
+                resetGame();
+            }
+        }
+    }
+
+    void render() {
+        window.clear(backgroundColor);
+
+        for (int c = 0; c < cols; c++) {
+            for (int r = 0; r < rows; r++) {
+                tile.setPosition({(c * tileSize) + spaceOutlineSize, (r * tileSize) + spaceOutlineSize});
+                window.draw(tile);
+
+                ball.setPosition({tile.getPosition().x + ballCenterPosition, tile.getPosition().y + ballCenterPosition});
+                ball.setFillColor(field[c][r].getColor());
+                window.draw(ball);
+            }
+        }
+
+        scoreText.setString(std::to_string(score));
+        window.draw(scoreText);
+
+        if (gameOver) {
+            window.draw(gameOverText);
+        }
+
+        if (grabedBall.type != Ball::Type::None) {
+            lastMousePosition = sf::Mouse::getPosition(window);
+            //printf("X - %d | Y - %d\n", lastMousePosition.x, lastMousePosition.y);
+            ball.setPosition({(float)lastMousePosition.x, (float)lastMousePosition.y});
+            ball.setFillColor(grabedBall.getColor());
+            window.draw(ball);
+        }
+
+        window.display();
+    }
+
+    void mouseClick() {
+        lastMousePosition = sf::Mouse::getPosition(window);
+        //printf("X - %d\tY - %d\n", lastMousePosition.x, lastMousePosition.y);
+
+        sf::Vector2i targetPosition;
+        targetPosition.x = lastMousePosition.x / tileSize;
+        targetPosition.y = lastMousePosition.y / tileSize;
+        
+        //printf("TargetPos : X - %d | Y - %d\n", targetPosition.x, targetPosition.y);
+
+        if ( 
+            ((targetPosition.x >= 0) && (targetPosition.y >= 0)) &&
+            ((targetPosition.x < cols) && (targetPosition.y < rows)) &&
+            (field[targetPosition.x][targetPosition.y].type != Ball::Type::None)
+        ) {
+            //puts("Grab!");
+            grabedBall.type = field[targetPosition.x][targetPosition.y].type;
+            field[targetPosition.x][targetPosition.y].type = Ball::Type::None;
+            window.setMouseCursorVisible(false);
+        } else if (
+            ((targetPosition.x >= 0) && (targetPosition.y >= 0)) &&
+            ((targetPosition.x < cols) && (targetPosition.y < rows)) &&
+            (grabedBall.type != Ball::Type::None)
+        ) {
+            //puts("Put!");
+            field[targetPosition.x][targetPosition.y].type = grabedBall.type;
+            grabedBall.type = Ball::Type::None;
+            window.setMouseCursorVisible(true);
+        }
+    }
+
+    void generateBalls() {
+        sf::Vector2i pos;
+        for (int i = 0; i < countGenerateBalls; i++) {
+            int j = 0;
+            do {
+                pos.x = rand() % cols;
+                pos.y = rand() % rows;
+                j++;
+                if(j > maxGeneratorIterations) {
+                    gameOver = true;
+                    return;
+                }
+            } while (field[pos.x][pos.y].type != Ball::Type::None);
+
+            field[pos.x][pos.y].type = Ball::Type((rand() % (Ball::Type::TypeCount - 1)) + 1);
+        }
+    }
+
+    void resetGame() {
+        score = 0;
+        gameOver = false;
+        for (std::vector<Ball> &row : field) {
+            for (Ball &ball : row) {
+                ball.type = Ball::Type::None;
+            }
+        }
+        grabedBall.type = Ball::Type::None;
+        generateBalls();
+    }
+
+    void checkField() {
+        for (int c = 1; c < cols - 1; c++) {
+            for (int r = 1; r < rows - 1; r++) {
+                
+            }
+        }
+    }
+};
+
+int main() {
+    Game *game = new Game();
+    game->run();
+    delete game;
+    return 0;
+}
---
+++ make.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ *.cpp -o run -lsfml-graphics -lsfml-window -lsfml-system
